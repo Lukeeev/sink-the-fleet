c8237778757a7a97f020e6f53fa43a1a2a2c254f
Lukeeev <luvacul@gmail.com>
2025-09-04 16:31:43 +0200

    backend init

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,13 @@
 import './App.css'
+import { Game } from './components/Game'
 import Menu from './components/Menu'
 
 
 function App() {
   return (
     <>
-      <div className='flex flex-row justify-center text-9xl font-lugrasimo mt-8 underline'>
-        Sink the fleet
-      </div>
       <Menu />
+      <Game />
     </>
   )
 }
---
+++ frontend/src/components/Game.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react"
+
+
+export const Game = () => {
+
+    const [grid, setGrid] = useState<number[][]>([
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0]
+    ])
+
+  return (
+    <>
+        <div className='flex flex-row justify-center text-9xl font-lugrasimo mt-8 underline'>
+        Sink the fleet
+        </div>
+        <div className="grid">
+            {grid.map((row, rowIndex) => (
+                <div key={rowIndex} className="row">
+                    {row.map((number, cellIndex) => (
+                        <div key={cellIndex} className="cell">{number}</div>
+                    ))}
+                </div>
+            ))}
+        </div>
+    </>
+  )
+}
+
